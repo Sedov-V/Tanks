e924f53a1bf1a685c509bd169d30e1732b5395ca
Sedov Vasiliy <v.sedov1999@yandex.ru>
2020-07-22 19:32:59 +0300

    First update

---
+++ Tanks/Entities/Tank.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    class Tank : Entity
+    {
+        public void OnCollision(Entity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
